c77627275d0bd69eb327492f74259a7475263cbc
Daniel <s3rek92@gmail.com>
2017-06-06 14:57:43 +0200

    dzialajaca wersja uslugi wyslajacej raporty miesieczne

--- WindowsService1/Service1.cs
+++ WindowsService1/Service1.cs
@@ -45,7 +45,7 @@ namespace WindowsService1
                 string mode = ConfigurationManager.AppSettings["Mode"].ToUpper();
                 this.WriteToFile("Simple Service Mode: " + mode + " {0}");
 
-                
+
 
                 //Set the Default Time.
                 DateTime scheduledTime = DateTime.MinValue;
@@ -103,6 +103,10 @@ namespace WindowsService1
 
             try
             {
+                //ustawianie dnia raportowania
+                string RaportDay = ConfigurationManager.AppSettings["RaportDay"].ToUpper();
+                this.WriteToFile("Simple Service Mode: " + RaportDay + " {0}");
+
                 DataSet ds = new DataSet();
                 DataTable dt = new DataTable();
                 string query2 = "SELECT * FROM xUsers WHERE Email IS NOT NULL";
@@ -134,8 +138,160 @@ namespace WindowsService1
                         sda.Fill(ds, "dPracePrzyProjektach");
                     }
                 }
+
+                /////wysłanie maili z brakami w raporcie
+                if (DateTime.Today.Day == Convert.ToInt32(RaportDay))
+                {
+                    string query4 = "SELECT Count(czy_roboczy)*8 AS m_godz FROM Kalendarz AS k WHERE DATEPART (yyyy, k.Data) = @Year AND DATEPART (m, k.Data) = @Month AND czy_roboczy='TRUE'";
+                    string query5 = "SELECT ID_uzytkownika, Sum(Datediff(ss, Godzina_od, Godzina_do) / 3600.0) AS m_rob FROM dPracePrzyProjektach WHERE Datepart(m, Data) = @Month AND Datepart(yyyy, Data) = @Year  Group BY ID_uzytkownika";
+                    string query6 = "SELECT ID_uzytkownika, Data, Sum(Datediff(mi, Godzina_od, Godzina_do) / 60.00) AS d_rob FROM dPracePrzyProjektach WHERE Datepart(m, Data) = @Month AND Datepart(yyyy, Data) = @Year Group BY ID_uzytkownika, Data";
+                    //string query6 = "SELECT dPracePrzyProjektach.ID_uzytkownika, Kalendarz.Data, Kalendarz.Dzien_tygodnia, Kalendarz.Czy_roboczy, Sum(Datediff(ss, dPracePrzyProjektach.Godzina_od, dPracePrzyProjektach.Godzina_do) / 3600.0) AS d_rob FROM Kalendarz LEFT OUTER JOIN dPracePrzyProjektach ON dPracePrzyProjektach.Data=Kalendarz.Data WHERE Datepart(m, Kalendarz.Data) = 5 AND Datepart(yyyy, Kalendarz.Data) = 2017 Group BY dPracePrzyProjektach.ID_uzytkownika, Kalendarz.Data, Kalendarz.Dzien_tygodnia, Kalendarz.Czy_roboczy ORDER BY Kalendarz.Data";
+                    string query7 = "SELECT Data, Dzien_tygodnia, Czy_roboczy FROM Kalendarz WHERE Datepart(m, Data) = @Month AND Datepart(yyyy, Data) = @Year";
+
+                    using (SqlCommand cmd = new SqlCommand(query5))
+                    {
+                        cmd.Connection = conn;
+                        cmd.Parameters.AddWithValue("@Month", DateTime.Today.Month - 1);
+                        cmd.Parameters.AddWithValue("@Year", DateTime.Today.Year);
+                        using (SqlDataAdapter mda = new SqlDataAdapter(cmd))
+                        {
+                            mda.Fill(ds, "rob_miesiac");
+                        }
+                    }
+                    using (SqlCommand cmd = new SqlCommand(query4))
+                    {
+                        cmd.Connection = conn;
+                        cmd.Parameters.AddWithValue("@Month", DateTime.Today.Month - 1);
+                        cmd.Parameters.AddWithValue("@Year", DateTime.Today.Year);
+                        using (SqlDataAdapter mda2 = new SqlDataAdapter(cmd))
+                        {
+                            mda2.Fill(ds, "miesiac");
+                        }
+                    }
+                    using (SqlCommand cmd = new SqlCommand(query6))
+                    {
+                        cmd.Connection = conn;
+                        cmd.Parameters.AddWithValue("@Month", DateTime.Today.Month - 1);
+                        cmd.Parameters.AddWithValue("@Year", DateTime.Today.Year);
+                        using (SqlDataAdapter mda = new SqlDataAdapter(cmd))
+                        {
+                            mda.Fill(ds, "rob_dni");
+                        }
+                    }
+                    using (SqlCommand cmd = new SqlCommand(query7))
+                    {
+                        cmd.Connection = conn;
+                        cmd.Parameters.AddWithValue("@Month", DateTime.Today.Month - 1);
+                        cmd.Parameters.AddWithValue("@Year", DateTime.Today.Year);
+                        using (SqlDataAdapter mda = new SqlDataAdapter(cmd))
+                        {
+                            mda.Fill(ds, "kalend");
+                        }
+                    }
+                    foreach (DataRow row in ds.Tables["xUsers"].Rows)
+                    {
+                        string messageBody = "<font>Poniżej zamieszczono wykaz z bazy za poprzedni miesiąc: </font><br><br>";
+                        string htmlTableStart = "<table style=\"border-collapse:collapse; text-align:center;\" >";
+                        string htmlTableEnd = "</table>";
+                        string htmlHeaderRowStart = "<tr style =\"background-color:#6FA1D2; color:#ffffff;\">";
+                        string htmlHeaderRowEnd = "</tr>";
+                        string htmlTrStart = "<tr style =\"color:#555555;\">";
+                        string htmlTrEnd = "</tr>";
+                        string htmlTdStart = "<td style=\" border-color:#5c87b2; border-style:solid; border-width:thin; padding: 5px;\">";
+                        string htmlTdStart2 = "<td style=\" border-color:#5c87b2; border-style:solid; border-width:thin; padding: 5px;background-color:#D3D3D3; color:#ff0000;\">";
+                        string htmlTdEnd = "</td>";
+
+                        messageBody += htmlTableStart;
+                        messageBody += htmlHeaderRowStart;
+                        messageBody += htmlTdStart + "Data" + htmlTdEnd;
+                        messageBody += htmlTdStart + "Dzień tygodnia" + htmlTdEnd;
+                        messageBody += htmlTdStart + "Ilość przepracowanych godzin " + htmlTdEnd;
+                        messageBody += htmlHeaderRowEnd;
+
+                        foreach (DataRow Rowk in ds.Tables["kalend"].Rows)
+                        {
+                            messageBody = messageBody + htmlTrStart;
+                            if ((Boolean)Rowk["Czy_roboczy"] == false)
+                            {
+                                string data = Rowk["Data"].ToString();
+                                messageBody = messageBody + htmlTdStart2 + data + htmlTdEnd;
+                                messageBody = messageBody + htmlTdStart2 + Rowk["Dzien_tygodnia"] + htmlTdEnd;
+                                messageBody = messageBody + htmlTdStart2 + htmlTdEnd;
+                            }
+                            else
+                            {
+                                string data = Rowk["Data"].ToString();
+                                messageBody = messageBody + htmlTdStart + data + htmlTdEnd;
+                                messageBody = messageBody + htmlTdStart + Rowk["Dzien_tygodnia"] + htmlTdEnd;
+                            }
+
+                            foreach (DataRow Rowr in ds.Tables["rob_dni"].Rows)
+                            {
+                                if (DateTime.Parse(Rowr["Data"].ToString()).ToShortDateString() == DateTime.Parse(Rowk["Data"].ToString()).ToShortDateString() & Rowr["Id_uzytkownika"].ToString() == row["ID"].ToString())
+                                {
+                                    messageBody = messageBody + htmlTdStart + Rowr["d_rob"] + htmlTdEnd;
+                                    messageBody = messageBody + htmlTrEnd;
+                                }
+                            }
+                        }
+                        string tekst = "";
+                        foreach (DataRow rowm in ds.Tables["rob_miesiac"].Rows)
+                        {
+                            if (rowm["Id_uzytkownika"].ToString() == row["ID"].ToString())
+                            {
+                                double m_rob = Convert.ToDouble(rowm["m_rob"]);
+                                double m_godz = Convert.ToDouble(ds.Tables["miesiac"].Rows[0][0]);
+                                if (m_rob > m_godz)
+                                {
+                                    double wydruk = Math.Round((m_rob - m_godz)*2)/ 2;
+                                    tekst = "w poprzednim miesiącu  przepracowałeś/aś " + m_rob + " godzin w tym masz nadgodziny w liczbie " + wydruk + " godzin";
+                                }
+                                if (m_rob < m_godz)
+                                {
+                                    double wydruk = Math.Round((m_godz - m_rob)*2)/ 2;
+                                    tekst = "w poprzednim miesiącu przepracowałeś/aś "+ m_rob +" godzin, ale masz za mało przepracowanych godzin o " + wydruk + " godziny";
+                                }
+                                if (m_rob == m_godz)
+                                {
+                                    tekst = "w poprzednim miesiącu masz przepracowane równe " + m_rob + " godziny";
+                                }
+                            }
+                        }
+                        messageBody = messageBody + htmlTableEnd + tekst;
+
+
+                        //string email = row["Email"].ToString();
+                        string email = "s3rek92@gmail.com";
+                        //if (email == row["Email"].ToString())
+                        {
+                            WriteToFile("Próba wysłania maila z raportem do: " + row["Email"]);
+
+                            using (MailMessage mm = new MailMessage("Unimap.katowice@gmail.com", email))
+                            {
+                                mm.Subject = "Miesięczna kontrola bazy";
+                                mm.IsBodyHtml = true;
+                                mm.Body = string.Format("<p>Witaj! " + row["Imie"] + " " + row["Nazwisko"] + "</p>" + messageBody);
+
+                                SmtpClient smtp = new SmtpClient();
+                                smtp.Host = "smtp.gmail.com";
+                                smtp.EnableSsl = true;
+                                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential();
+                                credentials.UserName = "Unimap.katowice@gmail.com";
+                                credentials.Password = "MalBry2015";
+                                smtp.UseDefaultCredentials = true;
+                                smtp.Credentials = credentials;
+                                smtp.Port = 587;
+                                smtp.Send(mm);
+                                WriteToFile("Email z raportem wysłany do: " + email);
+                            }
+                        }
+                    }
+                }
+                ///////////////////////////////////
+
                 bool rob = (from DataRow dr in ds.Tables["dPracePrzyProjektach"].Rows
                             select (bool)dr["czy_rob"]).FirstOrDefault();
+
                 if (rob)
                 {
 
@@ -173,7 +329,7 @@ namespace WindowsService1
                     foreach (DataRow row in ds.Tables["xUsers"].Rows)
                     {
 
-
+                        //string email = "s3rek92@gmail.com";
                         string email = row["Email"].ToString();
                         //string data = row["Data"].ToString();
                         string imie = row["Imie"].ToString();
